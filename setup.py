4366fbc4e8a80e17d416d06e64068688f932fa8a
denisesenguel <denisesenguel@gmail.com>
2021-02-01 13:15:16 +0000

    initialize repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Base image recognition model for land use based on Sentinel2 satellite images.',
+    author='Denise Sengül',
+    license='MIT',
+)

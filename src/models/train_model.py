3233d8f82dc7e01b084a559b0fcd0ce27100d7a4
denisesenguel <denisesenguel@gmail.com>
2021-03-12 17:00:27 +0100

    some more lines from tensorflow examples

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,7 +4,18 @@ from src.data.load_data import *
 
 def main():
 
-    data_split = preprocessImages()["training"]
+    data_processed = preprocessImages()
+
+    # the following bit is needed for larger testdata
+
+    # AUTOTUNE = tf.data.AUTOTUNE
+
+    # train_ds = data_processed["training"].cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    # val_ds = data_processed["validation"].cache().prefetch(buffer_size=AUTOTUNE)
+
+    data_split =
+
+
 
     np.random.seed(108)
 

7f8668267561aa7c1085f4cc81897981c977193f
denisesenguel <denisesenguel@gmail.com>
2021-03-12 16:59:59 +0100

    some fixes for auto-loading data

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,20 +1,34 @@
 import tensorflow as tf
 import pathlib
+import zipfile
+
 
 # this step can also be done manually
 def downloadRGB():
 
-    path = tf.keras.utils.get_file(
-        fname=pathlib.Path.cwd() / "data/raw/EuroSAT_RGB/",
+    path_to_zip = tf.keras.utils.get_file(
+        fname=pathlib.Path.cwd() / "data/raw/EuroSAT_RGB.zip",
         origin="http://madm.dfki.de/files/sentinel/EuroSAT.zip",
+        archive_format="zip",
         extract=True
     )
 
-    return path
+    # somehow the above fcn doesn't extract, seems to be a bug
+    # unzip manually
+    # this does not work properly yet
+    path_extracted = pathlib.Path(path_to_zip).parent / "/EuroSAT_RGB/"
+    with zipfile.ZipFile(path_to_zip, 'r') as zip_ref:
+        zip_ref.extractall(path_extracted)
+
+    return path_extracted
+
 
 def preprocessImages(batch_size=100, img_height=64, img_width=64):
 
-    path = downloadRGB()
+    path = pathlib.Path.cwd() / "data/raw/EuroSAT_RGB/"
+
+    # download only needs to be done once on every machine.
+    # path = downloadRGB()
 
     data_dict = dict()
 
@@ -23,8 +37,10 @@ def preprocessImages(batch_size=100, img_height=64, img_width=64):
         # set seed for split?
         data_dict[type] = tf.keras.preprocessing.image_dataset_from_directory(
             directory=path,
+            labels="inferred",
             subset=type,
             validation_split=0.2,
+            seed=149,
             image_size=(img_height, img_width),
             batch_size=batch_size
         )
